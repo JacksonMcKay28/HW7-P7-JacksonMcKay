e548b847706cda873adc4af1a9ac1050cd5e1dab
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-10-13 04:04:14 +0000

    code works but table is messed up

--- main.cpp
+++ main.cpp
@@ -3,19 +3,34 @@
 //Course: CSCI 130-001
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
   //set up arrays
-  const int R = 5;
-  double resistance[R] = {16, 27, 39, 56, 81};
+  double resistance[5] = {16, 27, 39, 56, 81};
   double currents[5];
   double power[5];
 
   //variable for total
   double total;
 
-  //for loop to ask for currents
-  for 
+  //for loop to ask for currents and to calc power
+  for (int i = 0; i < 5; i++) 
+  {
+    cout << "Enter values for current: ";
+    cin >> currents[i];
+    power[i] = resistance[i] * pow(currents[i],2);
+  }
+
+  //formatting the table
+  //headers with 20 spaces from start of word to start of next
+  cout << "Resistance          Current             Power" << endl;
+  //for loop to lay out all of the values from arrays
+  for (int i = 0; i < 5; i++) 
+  {
+    cout << resistance[i] << setw(20) << currents[i] << setw(15) << power[i];  
+  }
 }
